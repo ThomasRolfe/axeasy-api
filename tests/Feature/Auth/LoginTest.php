38005816d0b0a7e5154084dbc9bafb1806cd0578
Tom Rolfe <thomasrolfe@yahoo.co.uk>
2021-08-21 19:43:24 +0100

    Tests for user registration and ligin

---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withHeaders([
+            'Origin' => 'localhost',
+            'Referer' => 'localhost',
+            'Accept' => 'application/json'
+        ]);
+    }
+
+    public function test_user_can_log_in_with_correct_credentials()
+    {
+        $password = 'password';
+
+        $user = User::factory()->create([
+            'password' => Hash::make($password),
+        ]);
+
+        $this->get('/api/xsrf-cookie');
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password
+        ]);
+
+        $response->assertStatus(200);
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_can_not_log_in_with_incorrect_credentials()
+    {
+        $password = 'password';
+
+        $user = User::factory()->create([
+            'password' => Hash::make($password),
+        ]);
+
+        $this->get('/api/xsrf-cookie');
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => 'an_incorrect_password'
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('email');
+        $this->assertGuest();
+    }
+
+}
---
+++ tests/Feature/Auth/RegistrationTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class RegistrationTest extends TestCase
+{
+    use RefreshDatabase;
+    use WithFaker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->withHeaders([
+            'Origin' => 'localhost',
+            'Referer' => 'localhost',
+            'Accept' => 'application/json'
+        ]);
+    }
+
+    public function test_user_can_register()
+    {
+        $this->get('/api/xsrf-cookie');
+
+        $name = $this->faker->name;
+        $email = $this->faker->email;
+        $password = 'password';
+
+        $response = $this->post('/register', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => $password
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('Users', [
+            'email' => $email,
+            'name' => $name
+        ]);
+    }
+
+    public function test_user_cannot_register_with_wrong_password_confirmation()
+    {
+        $this->get('/api/xsrf-cookie');
+
+        $name = $this->faker->name;
+        $email = $this->faker->email;
+        $password = 'password';
+
+        $response = $this->post('/register', [
+            'name' => $name,
+            'email' => $email,
+            'password' => $password,
+            'password_confirmation' => 'wrong password'
+        ]);
+
+        $response->assertStatus(422);
+
+        $this->assertDatabaseMissing('Users', [
+            'email' => $email,
+            'name' => $name,
+            'email_verified_at' => null
+        ]);
+    }
+}
